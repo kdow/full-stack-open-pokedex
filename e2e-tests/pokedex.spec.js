d68186e5992275af3605e82a3d19242c18ad416e
Kelly Downes <kedownes@gmail.com>
2024-05-31 15:18:08 -0700

    Add e2e test for page navigation

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -8,4 +8,11 @@ describe('Pokedex', () => {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )).toBeVisible()
   })
+
+  test('pokemon page can be navigated to', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('wigglytuff')).toBeVisible()
+    await page.getByRole('link', { name: 'wigglytuff' }).click()
+    await expect(page.getByText('cute charm')).toBeVisible()
+  })
 })
